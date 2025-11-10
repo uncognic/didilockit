234e22ebdc03f98094d9b1ee0f57173b4f7c14de
uncognic <uncognic@proton.me>
2025-11-09 20:41:12 -0500

    added some metadata for f-droid

--- app/src/main/java/dev/uncognic/didilockit/MainActivity.kt
+++ app/src/main/java/dev/uncognic/didilockit/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.*
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.*
